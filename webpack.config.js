ca3d586c00d8004c0c91ac0502886fd53e94071b
qingnan.kong <bellakongqn@gmail.com>
2019-12-31 15:32:41 +0800

    style-loader css-loader less-loader 解析css

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports ={
             {
                 test:/.js$/,
                 use:'babel-loader'
+            },
+            {
+                test:/.less$/,
+                use:['style-loader','css-loader','less-loader']
             }
         ]
     }
